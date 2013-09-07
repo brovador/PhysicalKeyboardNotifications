60ae8844d8ceeb2148cf1fd74dec3b5b9c903d09
brovador <brovador@gmail.com>
2013-09-07 21:01:03 +0200

    [FIXED] Removing duplicated imports

--- Demo/AppDelegate.h
+++ Demo/AppDelegate.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "PhysicalKeyboardNotifications.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2013 Jesús. All rights reserved.
 //
 
-#import "PhysicalKeyboardNotifications.h"
 #import "ViewController.h"
 
 @implementation ViewController
